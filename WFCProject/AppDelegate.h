1fe2c7c4171f63716fe3a0dd043b6badba3cb5cb
wufer <wufan@huaxindata.com.cn>
2018-03-06 15:47:15 +0800

    Initial Commit

---
+++ WFCProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WFCProject
+//
+//  Created by wufer on 2018/3/6.
+//  Copyright © 2018年 wufer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
